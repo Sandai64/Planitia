c963fd8a46d28dff892c25e6f1662ec0a3fb7b1f
Erwan Egasse <contact@erwan.sh>
2024-01-15 11:28:33 +0100

    utils/myges: rewrite authenticate()

--- lib/utils/myges.dart
+++ lib/utils/myges.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:reskolae/models/user.dart';
 import 'package:reskolae/utils/logging.dart';
 import 'package:reskolae/utils/str.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 /// Reverse-engineered MyGES auth methods & other nice utilities.
 /// Thanks to tchenu aka TC.
@@ -16,7 +19,7 @@ class MyGES
   MyGES();
 
   // Helper methods
-  Future<dynamic> authenticate(String username, String password) async
+  Future<User?> authenticate(String username, String password) async
   {
     final Dio dioClient = Dio();
     dioClient.options.headers['Authorization'] = 'Basic ${Strutils.base64encode('$username:$password')}';
@@ -32,9 +35,30 @@ class MyGES
 
     final location = result.headers.value('Location')!.split('&');
 
-    final access_token = location[0].split('=')[1];
-    final expires_in   = int.parse(location[2].split('=')[1]);
+    final accessToken = location[0].split('=')[1];
+    final expiresIn   = int.parse(location[2].split('=')[1]);
 
-    inspect([access_token, expires_in]);
+    for (var elem in [username, accessToken, expiresIn, location])
+    {
+      Logging.log(this, 'login process : $elem');
+    }
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString('user.username', username);
+    prefs.setString('user.password', password);
+    prefs.setString('auth.token.value', accessToken);
+    prefs.setInt('auth.token.eol', expiresIn);
+    throw Exception();
+  }
+
+  Future<User?> getUserData(String authToken) async
+  {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Dio dioClient = Dio();
+
+    dioClient.options.headers['Authorization'] = 'Bearer ${prefs.getString('auth.token.value')}';
+
+
   }
 }
